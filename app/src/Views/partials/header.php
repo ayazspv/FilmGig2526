aa6d696edb783340dd3a38e9fb42ef758430f776
ayazspv <ayazsaadatpour2@gmail.com>
2026-04-04 23:00:51 +0200

    debugging styling

--- app/src/Views/partials/header.php
+++ app/src/Views/partials/header.php
@@ -10,7 +10,9 @@ $escape = static fn(string $value): string => htmlspecialchars($value, ENT_QUOTE
     <title><?= $escape((string) $pageTitle) ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" referrerpolicy="no-referrer">
-    <link rel="stylesheet" href="/assets/css/main.css">
+    <style>
+        <?php include __DIR__ . '/../../../public/assets/css/main.css'; ?>
+    </style>
 </head>
 <body style="background-color: #E5E7EB; color: #111827;">
     <!-- Header -->
